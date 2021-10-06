6a07593a21cdd18751cc84c05e8e5b27b7a6c3c0
Fazil Ahmed Shaikh <44942652+Fazil711@users.noreply.github.com>
2021-10-06 08:38:53 +0530

    Create databasetest.py

---
+++ databasetest.py
@@ -0,0 +1,22 @@
+import mysql.connector 
+
+con = mysql.connector.connect(host = "localhost",
+							  user = "root",
+							  passwd = "fazil@@621311",
+							  database = "testregister")
+
+cur = con.cursor()
+
+def login_user(tup):
+	try:
+		cur.execute("SELECT * FROM registeration WHERE user_name = %s AND passwd = %s;", tup)
+		return(cur.fetchone())
+	except:
+		return False
+
+def register_user(tup):
+	try:
+		cur.execute("INSERT INTO registeration (user_name, email_id, age, roll_no, phone_no, address, country, passwd) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", tup)
+		return(con.commit())
+	except:
+		return False
